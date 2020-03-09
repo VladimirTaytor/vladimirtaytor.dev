5eb7ae7fef5aea1cb90484782d2feffa1bc73fcd
Volodymyr Pihol <volodymyr.pihol@gmail.com>
2020-03-09 23:49:21 +0200

    Add mouse hovering tracking

--- src/client/engine/index.js
+++ src/client/engine/index.js
@@ -3,9 +3,15 @@ import * as THREE from 'three'
 let camera, scene, renderer
 let geometry, material, mesh
 
+let mouse, raycaster
+let isHovering = false
+
 function init() {
+  raycaster = new THREE.Raycaster()
+  mouse = new THREE.Vector2()
+
   camera = new THREE.PerspectiveCamera(
-    70,
+    50,
     window.innerWidth / window.innerHeight,
     0.01,
     10
@@ -27,13 +33,33 @@ function init() {
   document.body.appendChild(renderer.domElement)
 }
 
-function animate() {
-  requestAnimationFrame(animate)
+function update() {
+  raycaster.setFromCamera(mouse, camera)
 
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.02
+  // calculate objects intersecting the picking ray
+  var intersects = raycaster.intersectObjects(scene.children)
+
+  if (intersects.length) {
+    if (!isHovering) {
+      isHovering = true
+      document.body.style.cursor = 'pointer'
+    }
+  } else {
+    if (isHovering) {
+      document.body.style.cursor = 'default'
+      isHovering = false
+    }
+
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.02
+  }
 
   renderer.render(scene, camera)
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+  update()
 }
 
 function onWindowResize() {
@@ -43,9 +69,15 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
+function onMouseMove(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+}
+
 export function renderScene() {
   init()
   animate()
 
+  window.addEventListener('mousemove', onMouseMove, false)
   window.addEventListener('resize', onWindowResize, false)
 }
